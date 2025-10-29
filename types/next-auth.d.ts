6f9502a4974a5b3c1120da4908fcecdbcb0ff5fe
parlamas <parlamas@live.com>
2025-10-29 19:11:25 +0100

    Add NextAuth types and update gitignore

---
+++ types/next-auth.d.ts
@@ -0,0 +1,16 @@
+//types/next-auth.d.ts
+
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      role: string;
+    } & DefaultSession["user"];
+  }
+
+  interface User {
+    role: string;
+  }
+}
